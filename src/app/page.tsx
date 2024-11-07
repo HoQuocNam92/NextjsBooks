ce74a2641c2d9d223d9fb9d6ffb03b5c1cbf6981
hoquocnam92 <hoquocnam92@gmail.com>
2024-11-08 00:19:23 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,77 @@
-import Image from "next/image";
-
-export default function Home() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
+import banner from '../images/banner.jpg';
+import book01 from '../images/book-01.jpg';
+import book02 from '../images/book-02.jpg';
+import book03 from '../images/book-03.jpg';
+import book04 from '../images/book-04.png';
+import book05 from '../images/book-05.jpg';
+import book06 from '../images/book-06.jpg';
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div>
+        <div className="text-center mx-32  my-8 pb-4 border-b-2 border-gray-200 "  >
+          <div className="flex justify-between">
+            <div>
+              <h1 className="text-4xl font-bold">SHOPPING</h1>
+            </div>
+            <div className="">
+              <a className="mx-4" href="#">Home</a>
+              <a className="mx-4" href="#">Products</a>
+              <a className="mx-4" href="#">Services</a>
+              <a className="mx-4" href="#">Pricing</a>
+            </div>
+            <div className="flex items-center justify-end" >
+              <a className="mx-4 text-3xl" href=""><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i></a>
+              <a className="mx-4 text-3xl" href=""><i><FontAwesomeIcon icon={faCartShopping} /></i></a>
+              <a className="mx-4 text-3xl" href=""><i><FontAwesomeIcon icon={faUser} /></i></a>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div>
+          <Image className='w-[1250px] mx-auto  ' src={banner} alt="" />
+        </div>
+        <div className='grid grid-cols-3 w-[1250px] h-[1200px] mx-auto gap-[115px] mt-[100px] mb-[50px]'>
+          <div className='w-[340px] h-[538px] p-[15px] bg-gray-200'>
+            <Image className='w-[200px] h-[300px] mx-auto mt-[20px]' src={book01} alt="" />
+            <h4 className='font-bold text-[18px] my-[10px] ml-[20px]'>Tâm lý học về tiền</h4>
+            <h4 className='ml-[20px]'>Nghiên cứu cách mà tâm lý, cảm xúc và hành vi của con người ảnh hưởng đến cách họ kiếm, chi tiêu, tiết kiệm</h4>
+            <button className='bg-black text-white p-[10px] rounded-l mx-[105px] mt-[10px] hover:bg-white hover:text-black'>Mua ngay </button>
+          </div>
+          <div className='w-[340px] h-[538px] p-[15px] bg-gray-200' >
+            <Image className='w-[200px] h-[300px] mx-auto mt-[20px]' src={book02} alt="" />
+            <h4 className='font-bold text-[18px] my-[10px] ml-[20px]'>Thấu hiểu tiếp thị</h4>
+            <h4 className='ml-[20px]'>Một yếu tố quan trọng trong việc xây dựng và phát triển thương hiệu. Đây là quá trình tìm hiểu sâu sắc về thị trường.</h4>
+            <button className='bg-black text-white p-[10px] rounded-l mx-[105px] mt-[10px] hover:bg-white hover:text-black'>Mua ngay </button>
+          </div>
+          <div className='w-[340px] h-[538px] p-[15px] bg-gray-200'>
+            <Image className='w-[200px] h-[300px] mx-auto mt-[20px]' src={book03} alt="" />
+            <h4 className='font-bold text-[18px] my-[10px] ml-[20px]'>Từ tốt đến vĩ đại</h4>
+            <h4 className='ml-[20px]'>Cuốn sách nổi tiếng của Jim Collins, khám phá lý do tại sao một số công <br /> ty thành công vượt bậc  </h4>
+            <button className='bg-black text-white p-[10px] rounded-l mx-[105px] mt-[10px] hover:bg-white hover:text-black'>Mua ngay </button>
+          </div>
+          <div className='w-[340px] h-[538px] p-[15px] bg-gray-200'>
+            <Image className='w-[200px] h-[300px] mx-auto mt-[20px]' src={book04} alt="" />
+            <h4 className='font-bold text-[18px] my-[10px] ml-[20px]'>Nếu biết trăm năm là hữu hạn</h4>
+            <h4 className='ml-[20px]'>Mở đầu cho một suy tư sâu sắc về <br /> thời gian và cuộc sống. Đoạn thơ<br /> thường gợi ý đến những cảm xúc .</h4>
+            <button className='bg-black text-white p-[10px] rounded-l mx-[105px] mt-[10px] hover:bg-white hover:text-black'>Mua ngay </button>
+          </div>
+          <div className='w-[340px] h-[538px] p-[15px] bg-gray-200'>
+            <Image className='w-[200px] h-[300px] mx-auto mt-[20px]' src={book05} alt="" />
+            <h4 className='font-bold text-[18px] my-[10px] ml-[20px]'>Nghĩ giàu làm giàu</h4>
+            <h4 className='ml-[20px]'>Một cuốn sách nổi tiếng của tác giả Napoleon Hill, xuất bản lần đầu <br /> vào năm 1937.</h4>
+            <button className='bg-black text-white p-[10px] rounded-l mx-[105px] mt-[10px] hover:bg-white hover:text-black'>Mua ngay </button>
+          </div>
+          <div className='w-[340px] h-[538px] p-[15px] bg-gray-200'>
+            <Image className='w-[200px] h-[300px] mx-auto mt-[20px] ' src={book06} alt="" />
+            <h4 className='font-bold text-[18px] my-[10px] ml-[20px]'>7 thói quen để thành đạt</h4>
+            <h4 className='ml-[20px]'>Cuốn sách nổi tiếng của Stephen R. Covey, cung cấp các nguyên tắc cơ <br /> bản để phát triển cá nhân.</h4>
+            <button className='bg-black text-white p-[10px] rounded-l mx-[105px] mt-[10px] hover:bg-white hover:text-black'>Mua ngay </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,13 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+
       },
     },
   },
